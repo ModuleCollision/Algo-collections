9e06f4d3bcb25759c2db1d839b5c6102e87ee6ff
ModuleCollision <puyizhao@163.com>
2023-08-04 20:16:50 +0800

    update

---
+++ 基础/双指针 离散化 差分 前缀和/前缀和 二维前缀和/2023NC多校6_E.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef long double lb;
+const ll maxn = 1e5 + 5;
+const ll inf = 0x3f3f3f3f;
+const ll mod = 998244353;
+void solve() {
+	ll n, q; std::cin >> n >> q;
+	vector<ll>A(n + 5, 0);
+	for (ll i = 1; i <= n; i++)cin >> A[i];
+	vector<ll>pre(n + 5, 0);
+	ll cur = 0; std::set<ll>st;
+	vector<ll>cnt(n + 5); vector<ll>idx(n + 5, 0);
+	ll tot = 0;
+	for (ll i = 1; i <= n; i++) {
+		if (A[i] & 1)pre[i] = i, cur = i, st.insert(i), idx[i] = ++tot;
+		else pre[i] = cur;
+		cnt[i] += cnt[i - 1];
+		cnt[i] += (A[i] & 1);
+	}
+	st.insert(0);
+	cur = n + 1; vector<ll>suf(n + 5, 0);
+	for (ll i = n; i >= 1; i--) {
+		if (A[i] & 1)suf[i] = i, cur = i;
+		else suf[i] = cur;
+	}
+	st.insert(n + 1);
+	bool curr = 0;
+	vector ct(n + 5, array<ll, 2> {0, 0});
+	for (ll i = 1; i <= n; i++) {
+		ct[i][0] += ct[i - 1][0];
+		ct[i][1] += ct[i - 1][1];
+		if (A[i] & 1)curr ^= 1;
+		else {
+
+			ct[i][curr] += 1;
+		}
+	}
+	while (q--) {
+		ll l, r, k; std::cin >> l >> r >> k;
+		ll f = cnt[r] - cnt[l - 1];
+		if (r - l + 1 < k) {
+			puts("NO"); continue;
+		}
+		if (not f) {
+			puts("YES"); continue;
+		}
+		if (f & 1) {
+			puts("NO"); continue;
+		}
+		f /= 2;
+		auto p1 = st.lower_bound(l);
+		ll ws = idx[*p1]; ll pre = 0;
+		if (ws & 1) {
+			pre = ct[r][0] - ct[l - 1][0];
+		} else pre = ct[r][1] - ct[l - 1][1];
+		ll high = pre + f;
+		if (k > high)puts("NO");
+		else puts("YES");
+	}
+}
+signed main() {
+	ll T; std::cin >> T;
+	while (T --)
+		solve();
+}
