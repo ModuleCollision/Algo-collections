9c9c0d6e6706fe2100f9824702d8974bbf4045bb
ModuleCollision <puyizhao@163.com>
2023-10-16 08:15:12 +0800

    update

--- 动态规划/计数/LC100029.cpp
+++ 动态规划/计数/LC100029.cpp
@@ -23,3 +23,35 @@ public:
     return ans % mod;
   }
 };
+
+
+/*多重背包O(1)优化*/
+class Solution {
+public:
+  typedef long long ll;
+  const ll mod = 1e9 + 7;
+  int countSubMultisets(vector<int>& nums, int l, int r) {
+    int n = nums.size(); ll tot = accumulate(nums.begin(), nums.end(), 0ll);
+    vector<ll>dp(tot + 5, 0);
+    std::unordered_map<ll, ll>cnt;
+    for (auto x : nums)cnt[x]++;
+    dp[0] = cnt[0] + 1;/*多重背包问题求方案数*/
+    cnt.erase(0); r = min(r, (int)tot); ll sum = 0;
+    for (auto [x, y] : cnt) {
+      auto ndp = dp;
+      sum = min(sum + x * y, (ll)r);
+      for (ll j = x; j <= sum; j++) {
+        ndp[j] = (ndp[j] + ndp[j - x]) % mod;
+        if (j >= (y + 1)*x) {
+          ndp[j] = ((ndp[j] - dp[j - (y + 1) * x]) % mod + mod) % mod;
+        }
+      }
+      dp = ndp;
+    }
+    ll ans = 0;
+    for (int i = l; i <= r; i++) {
+      ans = (ans % mod + dp[i]) % mod;
+    }
+    return ans % mod;
+  }
+};
