2348505ea0b051a07717c06411d8c8f5a6dc2eb0
ModuleCollision <puyizhao@163.com>
2023-11-21 20:10:59 +0800

    update

---
+++ 数学/数论/题目/LGP2568.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+using namespace std;
+const i64 mod = 1e9 + 7;
+const i64 maxn = 1e7 + 5;
+const i64 inf = 0x3f3f3f3f3f3f3f3f;
+i64 prime[maxn], tot = 0; i64 e[maxn];
+bool vis[maxn]; i64 n; i64 pre[maxn];
+void init() {
+    e[1] = 1; pre[1] = e[1];
+    for (i64 i = 2; i <= n; i++) {
+        if (not vis[i]) {
+            prime[++tot] = i; e[i] = i - 1;
+        }
+        pre[i] = e[i] + pre[i - 1];
+        for (i64 j = 1; j <= tot and i * prime[j] <= n; j++) {
+            vis[i * prime[j]] = 1;
+            e[i * prime[j]] = e[i] * e[prime[j]];
+            if (i % prime[j] == 0) {
+                e[i * prime[j]] = e[i] * prime[j];
+                break;
+            }
+        }
+    }
+}
+void solve() {
+    cin >> n; init(); i64 ans = 0;
+    for (i64 i = 1; i <= tot; i++) {
+        i64 s = n / prime[i];
+        ans += 2 * pre[s] - 1;
+    }
+    cout << ans << "\n";
+}
+signed main() {
+    solve();
+}
---
+++ 数据结构/栈 单调栈 单调队列/LC2940.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+using namespace std;
+const i64 mod = 998244353;
+const i64 maxn = 1e6 + 5;
+const i64 inf = 0x3f3f3f3f3f3f3f3f;
+class Solution {
+public:
+  //单调递减栈
+  vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+    int n = heights.size(); int q = queries.size();
+    vector<int>ans(q);
+    vector<pair<int, int>>stk;//值单调减
+    vector<vector<pair<int, int>>>fq(n);
+    for (int i = 0; i < q; i++) {
+      int a = queries[i][1], b = queries[i][0];
+      if (b < a)swap(a, b);
+      fq[b].push_back({a, i});
+    }
+    for (int i = n - 1; i >= 0; i--) {
+
+      if (fq[i].size()) {
+        int b = i;
+        for (auto [a, idx] : fq[i]) {
+          if (heights[a] < heights[b]) {
+            ans[idx] = b; continue;
+          }
+          if ( a == b) {
+            ans[idx] = a; continue;
+          }
+          int id = lower_bound(stk.begin(), stk.end(), heights[a], [&](auto s, int  t)-> bool {
+            return s.first > t;
+          }) - stk.begin() - 1;
+          if (id < stk.size()) {
+            ans[idx] = stk[id].second;
+          } else {
+            ans[idx] = -1;
+          }
+        }
+      }
+      while (stk.size() and stk.back().first <= heights[i]) {
+        stk.pop_back();
+      }
+      stk.push_back({heights[i], i});
+    }
+    return ans;
+  }
+};
