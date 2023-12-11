54619b54084d3661cf5e9955f4d6cd0c94c90038
ModuleCollision <puyizhao@163.com>
2023-12-12 01:16:01 +0800

    update

--- 数据结构/线段树 树状数组/题目/LGP3373.cpp
+++ 数据结构/线段树 树状数组/题目/LGP3373.cpp
@@ -105,3 +105,140 @@ void solve() {
 signed main() {
 	solve();
 }
+
+
+
+
+/*多组Test下使用封装数据结构, 在函数中开对象,避免暴力清空数组造成时间浪费*/
+
+/*STL: 数据结构 cin cout function类 lowerbound builtin max min accumulate iota stoi atoi等函数需要加上std,避免命名空间污染*/
+
+/*数学先打表, 后对拍, 图论只会暴力就bitset搞*/
+
+/*300兆字节 1- 2 秒*/
+
+#include<bits/extc++.h>
+
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 1e5 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+struct Node {
+  i64 l, r; // 所维护的区间范围
+  i64 ls, rs;// 左右孩子
+  i64 mul, add, sum;
+
+} tr[maxn * 2 + 5]; i64 n, q, m, tot = 1, a[maxn];
+void pushup(i64 p) {
+  tr[p].sum = (tr[tr[p].ls].sum % m + tr[tr[p].rs].sum) % m;
+  return;
+}
+void pushdown(i64 p) {
+  i64 mid = (tr[p].l + tr[p].r) >> 1;
+  if (not tr[p].ls) {
+    tr[p].ls = ++tot;
+    tr[tot].l = tr[p].l;
+    tr[tot].r = mid;
+    tr[tr[p].ls].mul = 1;
+  }
+  if (not tr[p].rs) {
+    tr[p].rs = ++tot;
+    tr[tot].l = mid + 1;
+    tr[tot].r = tr[p].r;
+    tr[tr[p].rs].mul = 1;
+  }
+  if (tr[p].mul != 1) {
+    i64 mu = tr[p].mul;
+    tr[p].mul = 1;
+    tr[tr[p].ls].mul = tr[tr[p].ls].mul * mu % m;
+    tr[tr[p].rs].mul = (tr[tr[p].rs].mul * mu % m);
+    tr[tr[p].ls].sum = (tr[tr[p].ls].sum % m * mu) % m;
+    tr[tr[p].rs].sum = (tr[tr[p].rs].sum % m * mu) % m;
+    tr[tr[p].ls].add = (tr[tr[p].ls].add % m * mu) % m;
+    tr[tr[p].rs].add = (tr[tr[p].rs].add % m * mu) % m;
+  }
+  if (tr[p].add) {
+    i64 ad = tr[p].add;
+    tr[p].add = 0;
+    tr[tr[p].ls].add = (tr[tr[p].ls].add + ad) % m;
+    tr[tr[p].rs].add = (tr[tr[p].rs].add + ad) % m;
+    i64 mid = (tr[p].l + tr[p].r) >> 1;
+    i64 len1 = mid - tr[p].l + 1; i64 len2 = tr[p].r - mid;
+    tr[tr[p].ls].sum = (tr[tr[p].ls].sum % m + len1 % m * ad) % m;
+    tr[tr[p].rs].sum = (tr[tr[p].rs].sum % m + len2 % m * ad) % m;
+  }
+}
+void modify1(i64 p, i64 l, i64 r, i64 v) {
+  if (tr[p].l >= l and tr[p].r <= r) {
+    tr[p].mul = (tr[p].mul % m * v) % m;
+    tr[p].add = (tr[p].add % m * v) % m;
+    tr[p].sum = (tr[p].sum % m * v) % m;
+    return;
+  }
+  pushdown(p);
+  i64 mid = (tr[p].l + tr[p].r) >> 1;
+  if (l <= mid)modify1(tr[p].ls, l, r, v);
+  if (r > mid)modify1(tr[p].rs, l, r, v);
+  pushup(p);
+}
+
+void modify2(i64 p, i64 l, i64 r, i64 v) {
+  if (tr[p].l >= l and tr[p].r <= r) {
+    tr[p].add = (tr[p].add % m + v) % m;
+    tr[p].sum = (tr[p].sum + (tr[p].r - tr[p].l + 1) % m * v) % m;
+    return;
+  }
+  pushdown(p);
+  i64 mid = (tr[p].l + tr[p].r) >> 1;
+  if (l <= mid)modify2(tr[p].ls, l, r, v);
+  if (r > mid)modify2(tr[p].rs, l, r, v);
+  pushup(p);
+}
+
+i64 query(i64 u, i64 l, i64 r) {
+  if (tr[u].l >= l and tr[u].r <= r) {
+    return tr[u].sum % m;
+  }
+  pushdown(u);
+  i64 ret = 0;
+  i64 mid = (tr[u].l + tr[u].r) >> 1;
+  if (l <= mid)ret = (ret % m + query(tr[u].ls, l, r)) % m;
+  if (r > mid)ret = (ret % m + query(tr[u].rs, l, r)) % m;
+  return ret % m;
+}
+/*动态开点线段树*/
+int main() {
+  std::cin >> n >> q >> m;
+  tr[1].l = 1, tr[1].r = n;
+  tr[1].add = 0 , tr[1].mul = 1;
+  /*建树利用单点修改的方式*/
+  for (i64 i = 1; i <= n; i++)std::cin >> a[i], modify2(1, i, i, a[i]);
+  while (q--) {
+    i64 opt; std::cin >> opt;
+    if (opt == 1) {
+      i64 x, y, k; std::cin >> x >> y >> k;
+      modify1(1, x, y, k);
+    } else if (opt == 2) {
+      i64 x, y, k; std::cin >> x >> y >> k;
+      modify2(1, x, y, k);
+    } else {
+      i64 x, y; std::cin >> x >> y;
+      std::cout << query(1, x, y) % m << "\n";
+    }
+  }
+  return 0;
+}
